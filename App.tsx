7d0a2ccb17e8c79504c4fbd075417aeea2c9d7ab
gwenkit.com <build@gwenkit.com>
2024-06-22 12:36:48 +0900

    Hello, world!

--- App.tsx
+++ App.tsx
@@ -1,118 +1,174 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {Image, Button, Text, ScrollView, View} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const sampleImage = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64,
+};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const styles = {
+  container: {
+    flex: 1,
+    //paddingTop: 22,
+    //paddingBottom: 22,
+    backgroundColor: 'honeydew',
+  },
+  gutterTest: {
+    //paddingTop: 33,
+    backgroundColor: 'olive',
+  },
+  bottomTest: {
+    //paddingBottom: 33,
+    backgroundColor: 'olive',
+  },
+  scrollTest: {
+    //flex: 1,
+    //backgroundColor: 'white',
+  },
+  stringTest: {
+    paddingTop: 22,
+    paddingBottom: 22,
+    backgroundColor: 'white',
+  },
+  buttonTest: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type ButtonProps = {
+  label: string;
+  num: number;
+  numHide: boolean;
+};
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const SampleButtonGroup = () => {
+  const [msg, setMsg] = useState('ready!');
+  const resetCount = () => {
+    setMsg('Reset function is not ready yet. 개발 중...');
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <Text style={{
+        padding: 1,
+        fontSize: 14,
+        color: 'snow',
+        backgroundColor: 'purple',
+      }}>test(Button)</Text>
+      <View style={styles.buttonTest}>
+        <SampleButton label="Button" num="1" />
+      </View>
+      <View style={styles.buttonTest}>
+        <SampleButton label="Button" num="2" />
+        <SampleButton label="Button" num="3" />
+      </View>
+      <View style={styles.buttonTest}>
+        <SampleButton label="Button" num="4" />
+        <SampleButton label="Button" num="5" />
+        <SampleButton label="Button" num="6" />
+      </View>
+      <View style={styles.buttonTest}>
+        <SampleButton label="Button" num="7" />
+        <SampleButton label="Button" num="8" />
+        <SampleButton label="Button" num="9" />
+        <SampleButton label="Button" num="0" />
+      </View>
+      <View style={styles.buttonTest}>
+        <SampleButton label="A" num="A" numHide />
+        <SampleButton label="B" num="B" numHide />
+        <SampleButton label="C" num="C" numHide />
+        <SampleButton label="D" num="D" numHide />
+        <SampleButton label="E" num="E" numHide />
+        <SampleButton label="F" num="F" numHide />
+        <SampleButton label="G" num="G" numHide />
+        <SampleButton label="H" num="H" numHide />
+      </View>
+      <View style={styles.buttonTest}>
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+        }}>
+          <Button
+            onPress={resetCount}
+            title="Reset"
+          />
+          <Text style={{
+            padding: 2,
+            color: 'white',
+            backgroundColor: 'maroon',
+          }}>{msg}</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const SampleButton = (props: ButtonProps) => {
+  const [pressCount, setPressCount] = useState(0);
 
-export default App;
+  return (
+    <View style={{
+      flex: 1,
+      flexDirection: 'column',
+    }}>
+      <Button
+        onPress={() => {
+          setPressCount(pressCount+1);
+        }}
+        title={(props.num && !props.numHide) ? (
+          props.label + " #" + props.num
+        ) : (
+          props.label
+        )}
+      />
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+      }}>
+        <Text style={{
+          flex: 1,
+          fontSize: 12,
+          color: 'maroon',
+          textAlign: 'right',
+          padding: 2,
+          backgroundColor: 'lavender',
+        }}>#{props.num}:</Text>
+        <Text style={{
+          flex: 1,
+          fontSize: 14,
+          fontWeight: 'bold',
+          textAlign: 'left',
+          padding: 2,
+          backgroundColor: 'lavender',
+        }}>{pressCount}</Text>
+      </View>
+    </View>
+  );
+};
+
+const Pasta = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.gutterTest}>
+        <Image source={sampleImage} />
+      </View>
+      <ScrollView styles={styles.scrollTest}>
+        <SampleButtonGroup />
+      </ScrollView>
+      <View style={styles.stringTest}>
+        <Text style={{
+          color: 'darkolivegreen',
+        }}>Hello, world!</Text>
+      </View>
+      <View style={styles.bottomTest}>
+        <Image source={sampleImage} />
+      </View>
+    </View>
+  );
+};
+
+export default Pasta;
+
